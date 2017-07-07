aa56fdf171992c5ca4ae6336bf5269e7f2d53259
dabou <ch007m@gmail.com>
2017-07-07 10:20:39 +0200

    Add guides

---
+++ data/guides.js
@@ -0,0 +1,35 @@
+var movies = [
+    {
+        "id": 1,
+        "name": "HTTP Api",
+        "reference": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags": [
+            "Spring Boot",
+            "JPA"
+        ],
+        "category": [
+            "Spring Boot",
+            "Microservice"
+        ],
+        "date": "2017-01-01T10:00:00+00:00"
+    },
+    {
+        "id": 2,
+        "name": "Secured HTTP Api",
+        "reference": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags": [
+            "Spring Boot",
+            "JPA",
+            "SSO",
+            "Secured"
+        ],
+        "category": [
+            "Spring Boot",
+            "Microservice",
+            "Security"
+        ],
+        "date": "2017-01-01T10:00:00+00:00"
+    }
+]
